81d99398b9d40ca6c39d9bac2ea172671cb6d1fc
BenjaminSmith27 <60144457+BenjaminSmith27@users.noreply.github.com>
2020-05-12 17:08:43 -0400

    Update message.cpp

--- message.cpp
+++ message.cpp
@@ -3,6 +3,23 @@
 using namespace std;
 
 void message::printMessage(){
-  cout << "MakefileExample\n";
+  cout << "Player Price vs Current Value\n"
+    << "4.8 - 4.9 Pope\n"
+    << "7.5 - 7.8 Alexander-Arnold\n"
+    << "5.2 - 5.3 Gomez\n"
+    << "4.2 - 4.3 Taylor\n"
+    << "12.6 - 12.7 Salah\n"
+    << "6.1 - 6.1 Barnes\n"
+    << "8.2 - 8.5 Fernandez\n"
+    << "10.4 - 10.6 De Bruyne\n"
+    << "7.8 - 8.1 Jimenez\n"
+    << "6.4 - 6.4 Calvert Lewin\n"
+    << "10.9 - 11.1 Aubameyang\n"
+    << "3.9 - 3.9 Button\n"
+    << "4.3 - 4.3 Hayden\n"
+    << "4.0 - 4.1 Tangonga\n"
+    << "4.1 - 4.2 Kelly\n"
+
+    ;
 }
 
